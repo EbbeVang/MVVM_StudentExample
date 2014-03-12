f197ac95fd64154eb3d3176208ee4768541a831e
ev <ev@evang.dk>
2014-03-12 10:02:48 +0100

    added viewmodel

--- WpfApplication/ViewModel/Student.cs
+++ WpfApplication/ViewModel/Student.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using WpfApplication.Annotations;
 
 namespace WpfApplication.ViewModel
 {
-    class Student
+    class Student : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
