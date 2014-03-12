34435e797359c8d97354344cdc7ce777089a05b6
ev <ev@evang.dk>
2014-03-12 10:00:37 +0100

    Added project

---
+++ WpfApplication/Model/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication.Model
+{
+    class Student
+    {
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+
+        public String ClassName { get; set; }
+
+        public String ExamGroup { get; set; }
+
+    }
+}
